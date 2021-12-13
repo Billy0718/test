f126e84ac045035c98bd5c65a13438c835ada5cb
Billy0718 <95621034+Billy0718@users.noreply.github.com>
2021-12-13 20:43:54 +0800

    Update big.py

--- big.py
+++ big.py
@@ -1,15 +1,7 @@
 import streamlit as st
 
 st.write('歡迎進入猜數字遊戲，玩家們準備好了嗎？')
-GuessGame = st.number_input('是否要進入遊戲？(YES/NO)')
 confirm_input=st.button('輸入確認')
-if (GuessGame.lower() == "no"):
-    exit()
-elif (GuessGame.lower() == "yes"):
-    st.write("遊戲正式開始！")
-else:
-    exit()
-
 rang1 = st.number_input("請設定本局遊戲的最小值:")
 rang2 = st.number_input("請設定本局遊戲的最大值:")
 num = random.randint(rang1,rang2)
