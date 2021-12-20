149664eade260f57fee32629ef95dfd434d3d96f
Billy0718 <95621034+Billy0718@users.noreply.github.com>
2021-12-20 20:14:16 +0800

    Update Japanese.py

--- Japanese.py
+++ Japanese.py
@@ -13,10 +13,10 @@ confirm_input=st.button('请选择练习内容')
     2：片假名练习
     3：混合练习
 '''
-if confirm_input:
-  info2 ='''开始训练，按 ctrl+C 强行停止'''
+if confirm_input = (1,2,3):
+  st.write(开始训练，按 ctrl+C 强行停止)
 else:
-  输入错误 = "输入有误，请重新输入"
+  st.write(输入有误，请重新输入)
 
 st.write(info)
 tim1 = time.localtime(time.time())
